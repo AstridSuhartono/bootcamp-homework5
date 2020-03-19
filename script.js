ee689a84a9a342f8b693bf13151a08e445101aab
AstridSuhartono <astrid_sh@yahoo.com>
2020-03-19 23:41:45 +1030

    first iteration, testing how the style.css work. hardcode everything atm.

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    //set and display the current day and time
+    currentTime =  moment().format('LLLL');
+    $("#currentDay").text(currentTime);
+
+    //create an array of time
+
+
+});
