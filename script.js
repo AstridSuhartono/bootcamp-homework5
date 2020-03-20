cf48d3444f4c661059d58fc7c396fb21d5a2489c
AstridSuhartono <astrid_sh@yahoo.com>
2020-03-20 17:32:55 +1030

    fix the set colour of the background depends on time, try to solve saving into local database

--- script.js
+++ script.js
@@ -5,38 +5,77 @@ $(document).ready(function() {
     $("#currentDay").text(currentTime);
 
     //create an array of time
-    var timeArray = ["9 AM","10 AM","11 AM", "12 PM", "1 PM","2 PM","3 PM","4 PM","5 PM"]
+    var timeArray = ["9 AM","10 AM","11 AM", "12 PM", "1 PM","2 PM","3 PM","4 PM","5 PM"];
+   
+    //init();
+    renderTimeBlocks();
 
     //create the time blocks
-    for (var i = 0; i < timeArray.length; i++){
-        let timeBlock = $("<div></div>").addClass("time-block row");
-        let timeArea = $("<div></div>").addClass("hour col-2").text(timeArray[i]).attr("data-time",i + 9);
-        let textArea = $("<textarea></textarea>").addClass("description col-9");
-        let button = $("<button></button>").addClass("saveBtn col-1");
-        let saveIcon = $("<i></i>").addClass("fas fa-save fa-lg");
-        $(".container").append(timeBlock);
-        timeBlock.append(timeArea,textArea,button);
-        button.append(saveIcon);
-        
-        //set the colour of the text area
-        setRowColour(textArea,$(".hour").attr("data-time"));
-    }
+    function renderTimeBlocks(){
+        for (var i = 9; i <= 17; i++){
+            let index = i - 9;
+            let timeBlock = $("<div></div>").addClass("time-block row");
+            let timeArea = $("<div></div>").addClass("hour col-2").text(timeArray[index]);
+            let textArea = $("<textarea></textarea>").addClass("description col-9").text("").attr("data-inputIndex",index);
+            let button = $("<button></button>").addClass("saveBtn col-1").attr("data-btnIndex", index);
+            let saveIcon = $("<i></i>").addClass("fas fa-save fa-2x");
+            $(".container").append(timeBlock);
+            timeBlock.append(timeArea,textArea,button);
+            button.append(saveIcon);
+            
+            //set the colour of the text area
+            setRowColour(textArea,i);
 
-    //function to update the colour of the text area based on the time of the day
-    function setRowColour(row,hour){
-        if(hour < moment().format('H')){
-            row.addClass("past");
-        }else if(hour == moment().format('H')){
-            row.addClass("present");
-        }else{
-            row.addClass("future");
         }
     }
 
-    //get stored JSON string of toDo from local storage and parsing it back to an object
-    let toDo = JSON.parse(localStorage.getItem("toDo"));
+    //function to set the colour of the text area based on the time of the day
+    function setRowColour(row,hour){
+        if(hour < parseInt(moment().format('H'))){
+            row.addClass("past");
+        }else if(hour > parseInt(moment().format('H'))){
+            row.addClass("future");
+        }else{
+            row.addClass("present");
+        }
+    }
+
+    function init() {
+        // Get stored todos from localStorage
+        // Parsing the JSON string to an object
+            var storedTodos = JSON.parse(localStorage.getItem("todos"));
+        
+            // If todos were retrieved from localStorage, update the todos array to it
+            if (storedTodos !== null) {
+                todos = storedTodos;
+            } else{
+                todos = new Array();
+            }
+        }
 
 
+    function storeTodos() {
+        // Stringify and set "todos" key in localStorage to todos array
+        localStorage.setItem("todos", JSON.stringify(todos));
+      }
 
+    //when button is clicked
+    $(".button").on("click",function(){
+        let index = $(this).attr("data-btnIndex");
+        let inputId = $(".description").attr("data-inputIndex");
+        var todoText = inputId.text();
+        // Return from function early if submitted todoText is blank
+        if (todoText === "") {
+            return;
+        }
+
+        // Add new todoText to todos array
+        todos[index] = todoText;
+
+        // Store updated todos in localStorage, re-render the list
+        storeTodos();
+        renderTodos();
+
+    });
 
 });
